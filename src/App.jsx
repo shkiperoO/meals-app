e600e94a4a98df7be9b8507dadc3979502ac5266
shkiperoO <kravchuk.alex15@gmail.com>
2022-11-19 14:46:06 +0200

    Components create

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import './components/Favorites'
+import Favorites from './components/Favorites';
+import Meals from './components/Meals';
+import Modal from './components/Modal';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <main>
+      <Search/>
+      <Favorites/>
+      <Meals/>
+      <Modal/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Meals.jsx
@@ -0,0 +1,5 @@
+const Meals = () => {
+    return <h1>Meals Component</h1>
+}
+
+export default Meals
